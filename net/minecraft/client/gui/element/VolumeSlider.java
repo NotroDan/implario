b58d0f3a43e9d054b8488341d6b864ef1dd16da9
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-10-25 14:43:12 +0300

    v1.1.3

--- net/minecraft/client/gui/element/VolumeSlider.java
+++ net/minecraft/client/gui/element/VolumeSlider.java
@@ -1,6 +1,5 @@
 package net.minecraft.client.gui.element;
 
-import net.minecraft.Utils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.GlStateManager;
@@ -14,7 +13,8 @@ public class VolumeSlider extends SettingSlider {
 	public VolumeSlider(Settings settings, int x, int y) {
 		super(x, y, settings);
 		sliderPosition = settings.f();
-		height = 20;
+		height = 100;
+		width = 10;
 	}
 
 	public float getSliderPosition() {
@@ -31,29 +31,25 @@ public class VolumeSlider extends SettingSlider {
 	 */
 	@Override
 	protected void mouseDragged(Minecraft mc, int mouseX, int mouseY) {
-		if (this.visible) {
-			if (this.isMouseDown) {
-				this.sliderPosition = (float) (mouseY - this.yPosition / this.height);
+		if (!this.visible) return;
+		if (this.isMouseDown) {
+			this.sliderPosition = (float) (mouseY - this.yPosition) / this.height;
 
-				if (this.sliderPosition < 0.0F) {
-					this.sliderPosition = 0.0F;
-				}
+			if (this.sliderPosition < 0.0F) this.sliderPosition = 0.0F;
+			if (this.sliderPosition > 1.0F) this.sliderPosition = 1.0F;
 
-				if (this.sliderPosition > 1.0F) {
-					this.sliderPosition = 1.0F;
-				}
+			settings.set(sliderPosition);
 
-			}
-
-			int slider = (int) (yPosition - 20 * sliderPosition);
-
-			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-			drawRect(xPosition - 2, yPosition - 22, xPosition + 2, yPosition + 2, 0xb071ff51);
-			drawRect(xPosition - 2, slider, xPosition + 2, yPosition + 2, 0xc048a334);
-			drawRect(xPosition - 3, slider - 2, xPosition + 3, slider + 2, 0xd098ff82);
-//			this.drawTexturedModalRect(this.xPosition + (int) (this.sliderPosition * (float) (this.width - 8)), this.yPosition, 0, 66, 4, 20);
-//			this.drawTexturedModalRect(this.xPosition + (int) (this.sliderPosition * (float) (this.width - 8)) + 4, this.yPosition, 196, 66, 4, 20);
 		}
+
+		int slider = (int) (yPosition + height * sliderPosition);
+		int y = yPosition + height;
+
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+		drawHorizontalLine(xPosition, xPosition + 1, slider,-1);
+		drawHorizontalLine(xPosition, xPosition + 1, slider + 1,-1);
+		//			this.drawTexturedModalRect(this.xPosition + (int) (this.sliderPosition * (float) (this.width - 8)), this.yPosition, 0, 66, 4, 20);
+		//			this.drawTexturedModalRect(this.xPosition + (int) (this.sliderPosition * (float) (this.width - 8)) + 4, this.yPosition, 196, 66, 4, 20);
 	}
 
 	/**
@@ -63,15 +59,11 @@ public class VolumeSlider extends SettingSlider {
 	@Override
 	public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
 		if (!super.mousePressed(mc, mouseX, mouseY)) return false;
-		this.sliderPosition = (float) (mouseY - this.yPosition / this.height);
+		this.sliderPosition = (float) (mouseY - this.yPosition) / this.height;
 
-		if (this.sliderPosition < 0.0F) {
-			this.sliderPosition = 0.0F;
-		}
-
-		if (this.sliderPosition > 1.0F) {
-			this.sliderPosition = 1.0F;
-		}
+		if (this.sliderPosition < 0.0F) this.sliderPosition = 0.0F;
+		if (this.sliderPosition > 1.0F) this.sliderPosition = 1.0F;
+		settings.set(sliderPosition);
 
 		this.isMouseDown = true;
 		return true;
@@ -90,30 +82,12 @@ public class VolumeSlider extends SettingSlider {
 		GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
 		GlStateManager.blendFunc(770, 771);
 
+		int slider = (int) (yPosition + height * sliderPosition);
+		int y = yPosition + height;
+		drawRect(xPosition + 1, yPosition, xPosition + 9, y, 0xc0c7ffbf);
+		drawRect(xPosition + 1, slider, xPosition + 9, y, 0xff48a334);
 
-		if (Settings.FANCY_BUTTONS.b()) {
-
-			int color = i == 0 ? 0xd03f3f3f : 0xd04f4f4f;
-			int borderColor = 0xd0707070;
-			if (i == 2) {
-				if (Settings.RAINBOW_SHIT.b()) borderColor = Utils.rainbowGradient((int) (System.currentTimeMillis() % 1536));
-				else borderColor = 0xd0e06c14;
-			}
-			int border = 1;
-
-			drawRect(xPosition, yPosition, xPosition + width, yPosition + height, borderColor);
-			drawRect(xPosition + border, yPosition + border, xPosition + width - border, yPosition + height - border, color);
-
-		} else {
-			this.drawTexturedModalRect(this.xPosition, this.yPosition, 0, 46 + i * 20, this.width / 2, this.height);
-			this.drawTexturedModalRect(this.xPosition + this.width / 2, this.yPosition, 200 - this.width / 2, 46 + i * 20, this.width / 2, this.height);
-		}
 		this.mouseDragged(mc, mouseX, mouseY);
-		int j = 0xe0e0e0;
-		if (!enabled) j = 0xa0a0a0;
-		else if (hovered) j = 0xffffa0;
-
-		this.drawCenteredString(fontrenderer, this.displayString, this.xPosition + this.width / 2, this.yPosition + (this.height - 8) / 2, j);
 	}
 
 
