4d44174605cea83d4bd5d802b8f447b3c5e32fc0
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2019-05-09 15:23:38 +0300

    123

--- core/src/net/minecraft/enchantment/Enchantment.java
+++ core/src/net/minecraft/enchantment/Enchantment.java
@@ -13,7 +13,7 @@ public abstract class Enchantment {
 
     protected static final Enchantment[] enchantmentsList = new Enchantment[256];
     public static final Enchantment[] enchantmentsBookList;
-    static {Enchantments.class.getCanonicalName();}
+    static {Enchantments.getAll();}
     private static final Map<ResourceLocation, Enchantment> locationEnchantments = new HashMap<>();
 
 
