ac1fe75ced9228f7faba7257467fe8e2c504dbdf
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2019-05-24 18:42:02 +0300

    Removed production folder

--- production/console.bat
+++
@@ -1,3 +0,0 @@
-chcp 1251
-java -Xmx1G -Djava.library.path=gamedata/natives -cp gamedata\client-libs.jar;gamedata\client-back.jar;gamedata\client-front.jar Start --gameDir .\ --uuid 1a2b3c4d5e6f7d8d9d0d1a2b3c4d5e6d --userType mojang
-pause
