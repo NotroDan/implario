cb91b4fbeb2485448068cf30914a0266b5198263
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2019-05-29 00:15:12 +0300

    Исправлено некорректное отображение боссбара

--- vanilla/src/vanilla/ingame/ModuleBossStatus.java
+++ vanilla/src/vanilla/ingame/ModuleBossStatus.java
@@ -17,29 +17,29 @@ public class ModuleBossStatus implements Module {
 	@Override
 	public void render(GuiIngame gui, float partialTicks, ScaledResolution res) {
 
+		if (BossStatus.bossName == null || BossStatus.statusBarTime <= 0) return;
+
 		Profiler.in.startSection("bossHealth");
-		if (BossStatus.bossName != null && BossStatus.statusBarTime > 0) {
-			--BossStatus.statusBarTime;
-			AssetsFontRenderer fontrenderer = MC.getFontRenderer();
-			ScaledResolution scaledresolution = new ScaledResolution(MC.i());
-			int i = scaledresolution.getScaledWidth();
-			short short1 = 182;
-			int j = i / 2 - short1 / 2;
-			int k = (int) (BossStatus.healthScale * (float) (short1 + 1));
-			byte b0 = 12;
-			gui.drawTexturedModalRect(j, b0, 0, 74, short1, 5);
-			gui.drawTexturedModalRect(j, b0, 0, 74, short1, 5);
-			if (k > 0) gui.drawTexturedModalRect(j, b0, 0, 79, k, 5);
+		--BossStatus.statusBarTime;
+		AssetsFontRenderer fontrenderer = MC.getFontRenderer();
+		ScaledResolution scaledresolution = new ScaledResolution(MC.i());
+		int i = scaledresolution.getScaledWidth();
+		short short1 = 182;
+		int j = i / 2 - short1 / 2;
+		int k = (int) (BossStatus.healthScale * (float) (short1 + 1));
+		byte b0 = 12;
+		MC.getTextureManager().bindTexture(Gui.icons);
+		gui.drawTexturedModalRect(j, b0, 0, 74, short1, 5);
+		gui.drawTexturedModalRect(j, b0, 0, 74, short1, 5);
+		if (k > 0) gui.drawTexturedModalRect(j, b0, 0, 79, k, 5);
 
-			String s = BossStatus.bossName;
-			int l = 0xffffff;
+		String s = BossStatus.bossName;
+		int l = 0xffffff;
 
-			if (Config.isCustomColors()) l = CustomColors.getBossTextColor(l);
+		if (Config.isCustomColors()) l = CustomColors.getBossTextColor(l);
 
-			gui.getFontRenderer().drawStringWithShadow(s, (float) (i / 2 - gui.getFontRenderer().getStringWidth(s) / 2), (float) (b0 - 10), l);
-			G.color(1.0F, 1.0F, 1.0F, 1.0F);
-			MC.getTextureManager().bindTexture(Gui.icons);
-		}
+		gui.getFontRenderer().drawStringWithShadow(s, (float) (i / 2 - gui.getFontRenderer().getStringWidth(s) / 2), (float) (b0 - 10), l);
+		G.color(1.0F, 1.0F, 1.0F, 1.0F);
 		Profiler.in.endSection();
 	}
 
