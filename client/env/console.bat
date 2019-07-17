973fab4736431b593bd0a5d49d6659ced5b3869c
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2019-07-17 22:04:50 +0300

    Фикс запуска через .bat-файл

--- client/env/console.bat
+++ client/env/console.bat
@@ -1,3 +1,3 @@
 chcp 1251
-java -Xms128M -Xmx1G -Djava.library.path=gamedata/natives -cp gamedata\client-libs.jar;gamedata\client-back.jar;gamedata\client-front.jar Start --gameDir .\ --uuid 1a2b3c4d5e6f7d8d9d0d1a2b3c4d5e6d --userType mojang
+java -Xms128M -Xmx1G -Djava.library.path=gamedata/natives -cp gamedata\client.jar Start --gameDir .\ --uuid 1a2b3c4d5e6f7d8d9d0d1a2b3c4d5e6d --userType mojang
 pause
