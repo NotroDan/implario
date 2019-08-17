508e0e94009581f641bfe0c747a0b100e0137deb
6oogle <anartvel@gmail.com>
2019-08-17 03:16:48 +0300

    я случайно

--- vanilla/src/vanilla/entity/passive/EntityVillager.java
+++ vanilla/src/vanilla/entity/passive/EntityVillager.java
@@ -943,10 +943,8 @@ public class EntityVillager extends EntityAgeable implements IMerchant, INpc {
 	static class ListEnchantedBookForEmeralds implements EntityVillager.ITradeList {
 
 		public void modifyMerchantRecipeList(MerchantRecipeList recipeList, Random random) {
-			System.out.println(recipeList);
 			try {
-				System.out.println(Enchantment.enchantmentsBookList.length);
-				Enchantment enchantment = Enchantment.enchantmentsBookList[random.nextInt(Enchantment.enchantmentsBookList.length)];
+				Enchantment enchantment = Enchantment.enchantmentList.get(random.nextInt(Enchantment.enchantmentList.size()));
 				int i = MathHelper.getRandomIntegerInRange(random, enchantment.getMinLevel(), enchantment.getMaxLevel());
 				ItemStack itemstack = Items.enchanted_book.getEnchantedItemStack(new EnchantmentData(enchantment, i));
 				int j = 2 + random.nextInt(5 + i * 10) + 3 * i;
