73addd6a0924c818db4730c80208e1a514e73127
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2019-09-28 02:16:17 +0300

    + githubapi

--- core/src/net/minecraft/network/services/github/GitHubAPI.java
+++ core/src/net/minecraft/network/services/github/GitHubAPI.java
@@ -1,7 +1,12 @@
 package net.minecraft.network.services.github;
 
+import com.jsoniter.JsonIterator;
+import com.jsoniter.any.Any;
 import net.minecraft.network.services.Method;
 import net.minecraft.network.services.Request;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GitHubAPI {
 
@@ -11,8 +16,33 @@ public class GitHubAPI {
 		return new Request(ADDRESS + path, method).header("Authorization", "Client-ID d7044dce12f4f92");
 	}
 
+	public static List<Release> getReleases(String owner, String repo) {
+		Request request = request("repos/" + owner + "/" + repo + "/releases", Method.GET);
+		String json = request.execute();
+		Any any = JsonIterator.deserialize(json);
+		List<Release> list = new ArrayList<>();
+		for (Any release : any) {
+			List<GitHubAsset> assets = new ArrayList<>();
+			for (Any asset : release.get("assets")) {
+				assets.add(new GitHubAsset(asset.toString("name"), asset.toLong("size"), asset.toString("url")));
+			}
+			list.add(new Release(release.toString("tag"), release.toBoolean("prerelease"), release.toBoolean("draft"), assets));
+		}
+		return list;
+	}
 
-
+	public static void main(String[] args) {
+		long total = 0;
+		int count = 20;
+		for (int i = 0; i < count; i++) {
+			long start = System.currentTimeMillis();
+			for (Release release : getReleases("DelfikPro", "Implario")) {
+			}
+			long end = System.currentTimeMillis();
+			total += end - start;
+		}
+		System.out.println(total / count + "ms");
+	}
 
 //	public static String postImage(BufferedImage image) {
 //
