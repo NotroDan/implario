4331da078a5c0ac1cac9563d569185106c048dbf
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2020-01-24 18:27:24 +0300

    Fix

--- core/src/net/minecraft/network/protocol/implario/login/NetHandlerLoginServer.java
+++ core/src/net/minecraft/network/protocol/implario/login/NetHandlerLoginServer.java
@@ -10,9 +10,13 @@ import net.minecraft.network.protocol.minecraft.login.server.S02PacketLoginSucce
 import net.minecraft.network.protocol.minecraft.login.server.S03PacketEnableCompression;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.chat.ChatComponentText;
+import org.apache.commons.lang3.Validate;
 
 public class NetHandlerLoginServer extends net.minecraft.network.protocol.minecraft.login.NetHandlerLoginServer implements INetHandlerLoginServerImplario {
-    public NetHandlerLoginServer(MinecraftServer server, NetworkManager networkManager) {
+
+	private C02PacketClientInfo infoPacket;
+
+	public NetHandlerLoginServer(MinecraftServer server, NetworkManager networkManager) {
         super(server, networkManager);
     }
 
@@ -22,8 +26,15 @@ public class NetHandlerLoginServer extends net.minecraft.network.protocol.minecr
         networkManager.sendPacket(new S04PacketServerInfo());
     }
 
-    @Override
+	@Override
+	public void update() {
+		if (infoPacket != null) tryAcceptPlayer();
+		else super.update();
+	}
+
+	@Override
     public void processClientInfo(C02PacketClientInfo packetIn) {
-        tryAcceptPlayer();
+		Validate.validState(infoPacket == null, "Info packet sent twice");
+		this.infoPacket = packetIn;
     }
 }
